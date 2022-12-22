fdea6d2f39baa24fe2558498d525f92c06d889c4
graymigii <abdullahkassa@gmail.com>
2022-12-22 16:40:53 +0100

    done

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-const maxCanvasSize = 70;
-
-const createCanvas = (numberOffSquares) => {
+const createCanvas = (numberOfSquares) => {
+    const maxCanvasSize = 70;
     const container = document.querySelector('.container');
-    for(let i = 0; i < numberOffSquares; i++){
+    for(let i = 0; i < numberOfSquares; i++){
         const column = document.createElement('div');
         column.classList.add('column');
         column.style.display = 'flex';
-        for (let j = 0; j < numberOffSquares; j++){
+        for (let j = 0; j < numberOfSquares; j++){
             const row = document.createElement('div');
             row.classList.add('row');
-            //row.style.flex = '0 0 auto';
-            row.style.width = `${ maxCanvasSize / numberOffSquares }vh`;
-            row.style.height = `${ maxCanvasSize / numberOffSquares }vh`;
+            row.style.width = `${ maxCanvasSize / numberOfSquares }vh`;
+            row.style.height = `${ maxCanvasSize / numberOfSquares }vh`;
             column.appendChild(row);
         }
         container.appendChild(column);
     }
 }
 
-createCanvas(32);
-
-const rows = document.querySelectorAll('.row');
-
 let currentColor = 'blue';
+const sizeChangerBtn = document.querySelector('.size-changer');
+const resetBtn = document.querySelector('.reset');
+const container = document.querySelector('.container');
 
-rows.forEach(row => {
-    row.addEventListener('mouseover', () => {
-        row.style.backgroundColor = currentColor;
+const deleteCanvas = () => {
+    const columns = document.querySelectorAll('.column');
+    columns.forEach(column => {
+        container.removeChild(column);
     });
+}
+
+const resetSquares = () => {
+    const rows = document.querySelectorAll('.row');
+    resetBtn.addEventListener('click', () => {
+        rows.forEach(row => {
+            row.style.backgroundColor = 'white';
+        });
+    });
+    
+}
+
+const colorSquares = () => {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.addEventListener('mouseover', () => {
+            row.style.backgroundColor = currentColor;
+        });
+    });
+}
+
+sizeChangerBtn.addEventListener('click', () => {
+    const numberOfSquares = prompt('Enter the number of squares you want: ');
+    deleteCanvas();
+    createCanvas(numberOfSquares);
+    colorSquares();
+    resetSquares();
 });
+
+
+
+createCanvas(16);
+colorSquares();
+resetSquares();
